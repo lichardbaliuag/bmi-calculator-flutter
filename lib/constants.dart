0e3fed7b23db31b300739d9414a23b13098c1d15
Lichard Baliuag <baliuag_lichard@yahoo.com>
2019-12-03 23:06:05 +1300

    Completed BMI Project

---
+++ lib/constants.dart
@@ -0,0 +1,42 @@
+
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+const kBottomContainerColour = Color(0xFF00C853);   //0xFFEB1555 #00c853
+const kInactiveCardColour = Color(0xFF111328);
+const kActiveCardColour = Color(0xFF1D1E33);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kNumbertextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontWeight: FontWeight.bold,
+  fontSize: 22.0,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
